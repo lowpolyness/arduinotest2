443a2a529043dde9456472ee51d4a9159216601e
Michael Santina <michaelsantina@gmail.com>
2022-07-23 21:38:41 -0700

    LCD Sin Wave

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,43 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <math.h>
+#include <string>
+#include <Fonts/FreeMonoBold18pt7b.h>
 
-/*
-  Blink
 
-  Turns an LED on for one second, then off for one second, repeatedly.
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3D ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
+String greeting = "BUTTS";
 
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
-*/
-
-// the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Address 0x3C for 128x32
+  
+  display.setFont(&FreeMonoBold18pt7b);
+  display.clearDisplay();
+  display.display();
+  //display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+
 }
 
-// the loop function runs over and over again forever
-void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(100);                       // wait for a second
+void loop() {  
+
+  for(unsigned int c = 0; c < greeting.length(); ++c){
+    int vcursor = 20 * sin(.5 * millis()) + 40;
+    display.setCursor(c* 22, vcursor);
+    display.print(greeting[c]);
+     // actually display all of the above
+  }
+  display.display();
+  delay(100);
+  display.clearDisplay();
 }
