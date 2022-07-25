d6cba0f9f0f0a035ae762544d6faa0fcd5d704b0
Michael Santina <michaelsantina@gmail.com>
2022-07-24 22:51:07 -0700

    Added some vars to replace magic #s

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,8 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 const int potPin = A1;
 
+const int frame_delay = 50;
+
 unsigned long currentTime;
 unsigned long nextTime;
 
@@ -34,22 +36,22 @@ void setup() {
   display.display();
   display.setTextColor(SSD1306_WHITE);
   currentTime = millis();
-  nextTime = currentTime + 100;
+  nextTime = currentTime + frame_delay;
 }
 
 void loop() {
   currentTime = millis();
 
   if (currentTime == nextTime) {
-    float fval = 1 - map(analogRead(potPin), 0, 1023, 0, 100) / 100.0f;
+    float fval = 1 - map(analogRead(potPin), 0, 1023, 0, 100) / 100.0f;  //Map the range of the pot to 1-100, divide by float 100 to get value 0-1, 1-value to invert pot direction
     for(unsigned int c = 0; c < greeting.length(); ++c){
-      int vcursor = (fval * 20) * sin(.8 * millis()) + 40;
+      int vcursor = (fval * 20) * sin(.8 * millis()) + 40;  //Fval scales 20, the max range. .8 is a good value for a smooth sine wave. Add 40 to adjust vertical starting point
       display.setCursor(c* 22, vcursor);
       display.print(greeting[c]);
     }
     display.display();
     display.clearDisplay();
-    nextTime = millis() + 50;
+    nextTime = millis() + frame_delay;
   }
 }
 
